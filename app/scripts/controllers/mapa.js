b2ae5575ccda3831c26ca2b835161818a1e2f9c7
thulioph <thulioph@gmail.com>
2016-04-10 17:46:05 -0300

    Ajustes na tela de login e no header, remove imagens não utilizadas.

--- app/scripts/controllers/mapa.js
+++ app/scripts/controllers/mapa.js
@@ -30,18 +30,68 @@ angular.module('hortalivreApp')
 
     // ====
     // Initialize map with user location
-    function initMap(position) {
-      var userPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var map, userPosition, marker, infowindow, bounds, south_lat, south_lng,
+        north_lat, north_lng, center_lat, center_lng;
 
-      var map = new google.maps.Map(document.getElementById('map-container'), {
+    function initMap(position) {
+      userPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      map = new google.maps.Map(document.getElementById('map-container'), {
         zoom: 12,
         center: userPosition,
         scrollwheel: false
       });
 
-      var marker = new google.maps.Marker({
+      marker = new google.maps.Marker({
         map: map,
-        position: userPosition
+        position: userPosition,
+        icon: 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-32.png'
+      });
+
+      infowindow = new google.maps.InfoWindow({
+        content: 'Hey You',
+        maxWidth: 700
+      });
+
+      // evento para quando mexer no mapa
+      // google.maps.event.addListener(map, 'idle', showMarkers);
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+      });
+    }
+
+    // Show new marker when move map
+    function showMarkers() {
+     bounds = map.getBounds();
+
+     // south = map.getBounds().getSouthWest();
+     south_lat = map.getBounds().getSouthWest().lat();
+     south_lng = map.getBounds().getSouthWest().lng();
+
+     // north = map.getBounds().getNorthEast();
+     north_lat = map.getBounds().getNorthEast().lat();
+     north_lng = map.getBounds().getNorthEast().lng();
+
+     center_lat = (south_lat + north_lat) / 2;
+     center_lng = (south_lng + north_lng) / 2;
+
+      // console.warn('center_lat -> ', center_lat);
+      // console.warn('center_lng -> ', center_lng);
+
+      infowindow = new google.maps.InfoWindow({
+        content: 'lat: ' + center_lat + ', lng: ' + center_lng,
+        maxWidth: 700
+      });
+
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(center_lat, center_lng),
+        map: map
+      });
+
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
       });
     }
     // ====
